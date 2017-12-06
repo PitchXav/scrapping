5dc3f43db88aac18b34db4b383f655908e8fe7b3
Xavier <xvalet@regionsjob.com>
2017-12-06 11:36:41 +0100

    encode

--- scraper/spiders/alltricks.py
+++ scraper/spiders/alltricks.py
@@ -54,7 +54,7 @@ class alltricksSpider(scrapy.Spider):
         def findCritere(liste, texte):
             retour = 'n.c'
             for word in liste:
-                if re.search(word.decode('utf-8') , texte.decode('utf-8') , re.IGNORECASE):
+                if re.search(word.encode('utf-8') , texte.encode('utf-8') , re.IGNORECASE):
                     retour = word
             return retour
 
