3348e830fd56959d54fc61c2fb3998bc60bb54e5
Xavier <xvalet@regionsjob.com>
2017-12-07 16:51:06 +0100

    if not

--- scraper/spiders/alltricks.py
+++ scraper/spiders/alltricks.py
@@ -113,31 +113,31 @@ class alltricksSpider(scrapy.Spider):
         print textaAnalyser
 
         item['universVelo'] = findCritere(univers, item['titreVelo'])
-        if not (item['universVelo'])
+        if not (item['universVelo']):
             item['universVelo'] = findCritere(univers, item['descriptionVelo'])
 
         item['cadreVelo'] = findCritere(cadre, item['titreVelo'])
-        if not (item['cadreVelo'])
+        if not (item['cadreVelo']):
             item['cadreVelo'] = findCritere(cadre, item['descriptionVelo']) 
 
         item['styleVelo'] = findCritere(style, item['titreVelo'])
-        if not (item['styleVelo'])
+        if not (item['styleVelo']):
             item['styleVelo'] = findCritere(style, item['descriptionVelo']) 
 
         item['pratiqueVelo'] = findCritere(pratique, item['titreVelo'])
-        if not (item['pratiqueVelo'])
+        if not (item['pratiqueVelo']):
             item['pratiqueVelo'] = findCritere(pratique, item['descriptionVelo']) 
 
         item['genreVelo'] = findCritere(genre, item['titreVelo'])
-        if not (item['genreVelo'])
+        if not (item['genreVelo']):
             item['genreVelo'] = findCritere(genre, item['descriptionVelo']) 
 
         item['matieriauxVelo'] = findCritere(materiaux, item['titreVelo'])
-        if not (item['matieriauxVelo'])
+        if not (item['matieriauxVelo']):
             item['matieriauxVelo'] = findCritere(materiaux, item['descriptionVelo']) 
 
         item['tailleRoueVelo'] = findCritere(roues, item['titreVelo'])
-        if not (item['tailleRoueVelo'])
+        if not (item['tailleRoueVelo']):
             item['tailleRoueVelo'] = findCritere(roues, item['descriptionVelo']) 
         
 
